bd9fed71cf11e7400eb2549cffb753042310277e
luizcastilho2ds <luizota2009@gmail.com>
2026-03-19 10:28:29 -0300

    Create classificador.py

---
+++ classificador.py
@@ -0,0 +1,16 @@
+# Solicita um número ao usuário
+numero = float(input("digite um numero"))
+
+# Verifica se o número é positivo
+if numero > 0:
+    # Se for positivo, imprime a mensagem
+    print("O número é positiva.")
+# Verifica se o número é negativo
+elif numero < 0:
+    # Se for negativo, imprime a mensagem
+    print("O número é negatativo")
+# Se não for positivo nem negativo, é zero
+else:
+    # Imprime a mensagem para zero
+    print("o número é 0")
+
